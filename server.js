9cf1a684c31f572d85d2b6ddccebf79757dfc255
Jimmm12 <trantienhuy1112@gmail.com>
2024-10-25 23:44:57 +0700

    Update Api link

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./config/connectDB');
-const dotenv = require('dotenv');
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/connectDB");
+const dotenv = require("dotenv");
 const app = express();
-const authRouter = require('./routes/auth');
-const courseRouter = require('./routes/course');
-const userRouter = require('./routes/user');
-const orderRouter = require('./routes/order');
+const authRouter = require("./routes/auth");
+const courseRouter = require("./routes/course");
+const userRouter = require("./routes/user");
+const orderRouter = require("./routes/order");
 dotenv.config();
 connectDB();
-
-
 
 app.use(cors());
 app.use(express.json());
 
+//ROUTER
+app.use("https://course-backend-1akf.onrender.com/v1/auth", authRouter);
+app.use("https://course-backend-1akf.onrender.com/v1/course", courseRouter);
+app.use("https://course-backend-1akf.onrender.com/v1/user", userRouter);
+app.use("https://course-backend-1akf.onrender.com/v1/order", orderRouter);
 
-//ROUTER 
-app.use('https://course-backend-1akf.onrender.com/v1/auth',authRouter);
-app.use('https://course-backend-1akf.onrender.com/v1/course',courseRouter);
-app.use('https://course-backend-1akf.onrender.com/v1/user',userRouter);
-app.use('https://course-backend-1akf.onrender.com/v1/order',orderRouter);
-
-app.listen(8000, ()=> {
-  console.log("Server is running....")
-})
+app.listen(process.env.PORT || 8000, () => {
+  console.log("Server is running....");
+});
